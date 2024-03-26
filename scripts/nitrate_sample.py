f0c310024cc5bff201d4c479c1f00d77f7f680d3
Sruthi Mukkamala <sruthim@andrew.cmu.edu>
2024-03-25 20:22:07 -0700

    v2 w/ comments

---
+++ scripts/nitrate_sample.py
@@ -0,0 +1,148 @@
+"""
+File: nitrate_sample.py
+Author: Sruthi Mukkamala
+Date: 3/11/2024
+Description: Handles function calls and serial communication between end effector's Adafruit Bluefruit and NiMo's Finite State Machine (FSM).
+"""
+
+#!/usr/bin/env python3
+
+from nimo_end_effector.srv import *
+import rospy
+
+from serial import *
+import time
+
+class NSampleNode:
+    @classmethod
+    def __init__(cls):
+        """
+        The end effector node has three service calls - 
+        (1) get_dat: receive nitrate sensor's readings (when inserted in cornstalk) in mV. Complete simple data processing and linear interpolation 
+        to determine final nitrate concentration reading in ppm (parts per million).
+        (2) get_cal_dat: pass into service call which calibration point to collect and store: high concentration or low concentration. Similar to get_dat,
+        mV values are recorded from the nitrate sensor. Data stored is the raw mV reading at this calibration point.
+        (3) act_linear: pass into service call whether to fully extend or rectract linear actuator for gripping mechanism. 
+        """
+
+        # Define service calls
+        rospy.loginfo('Starting nitrate_sample node.')
+        cls.get_dat_service = rospy.Service('get_dat', get_dat, cls.handle_get_dat)
+        cls.get_cal_dat_service = rospy.Service('get_cal_dat', get_cal_dat, cls.handle_get_cal_dat)
+        cls.act_linear_service = rospy.Service('act_linear', act_linear, cls.handle_act_linear)
+
+        # Define global variables
+        cls.cal_high = -1 #high calibration reading, in mV
+        cls.cal_low = -1 #low calibration reading, in mV
+        cls.conc_high = 2000 #high calibration concentration of sol'n, in ppm
+        cls.conc_low = 200 #low calibration concentration of sol'n, in ppm
+
+        # Set up pyserial communication
+        cls.serialcomm = Serial('/dev/ttyACM0', 9600)
+        cls.serialcomm.timeout = 1
+
+        rospy.loginfo('Waiting for service calls...')
+
+    @classmethod
+    def sample(cls):
+        """
+        Handles reading nitrate sensor readings and simple data processing. Data processing involves taking the average of the readings
+        over a course of 10 seconds.
+        """
+
+        # Delay reading for 5 seconds
+        time.sleep(5)
+        # Read continuously for 10 seconds
+        t_end = time.time() + 10
+        n_samples = 0
+        nit_val = 0
+        # For ten seconds, collect data and sample values in mV
+        while time.time() < t_end:
+            nit_val += float(cls.serialcomm.readline())
+            n_samples += 1
+        # Average over ten seconds for final calibration value in mV
+        nit_val /= n_samples
+        # Return nitrate sensor values in mV
+        return nit_val
+    
+    @classmethod
+    def handle_get_dat(cls, req: get_datRequest) -> get_datResponse:
+        """
+        get_dat callback function. Reads in nitrate sensor values and linearly interpolates to ppm value.
+
+        INPUTS (get_datRequest):
+        * None
+        OUTPUTS (get_datResponse):
+        * flag: description of how get_dat process went
+        * nitrate_val: nitrate concentration reading from nitrate sensor, in ppm. -1 if invalid or out of range reading.
+        """
+
+        # Call sample function
+        nit_val_mV = cls.sample()
+        # Linear interpolation for value in ppm (given mV)
+        nit_val_ppm = (cls.conc_high-cls.conc_low)/(cls.cal_high-cls.cal_low)*(nit_val_mV-cls.cal_low) + cls.conc_low
+        # If interpolated value is out of calibration range, throw error flag
+        if nit_val_ppm > cls.conc_high or nit_val_ppm < cls.conc_low:
+            return get_datResponse(nitrate_val = -1, flag = "Nitrate value out of range.")
+        # Otherwise return nitrate value in ppm
+        else:
+            return get_datResponse(nitrate_val = nit_val_ppm, flag = "SUCCESS")
+        
+    @classmethod
+    def handle_get_cal_dat(cls, req: get_cal_datRequest) -> get_cal_datResponse:
+        """
+        get_cal_dat callback function. Reads in nitrate sensor values during calibration process and stores as raw mV value.
+
+        INPUTS (get_cal_datRequest):
+        * status: identifies which clean/calibration process is triggered by FSM. Can be "clean" (water rinse), "cal_low" (low conc. calibration process),
+        or "clean_high" (low conc. calibration process).
+        OUTPUTS (get_cal_datResponse):
+        * flag: description of how get_cal_dat process went
+        """
+
+        status = req.status
+        # If "clean" status, just return N/A flag
+        if status == "clean":
+            rospy.loginfo("Clean sensor has been requested.")
+            return get_cal_datResponse(flag = "N/A")
+        # If "cal_low" or "cal_high", call sample function and store appropriately
+        elif status == "cal_low":
+            cls.cal_low = cls.sample()
+        elif status == "cal_high":
+            cls.cal_high = cls.sample()
+        # Throw error otherwise
+        else:
+            rospy.logerr("Invalid status for calibration has likely been inputted!", "red")
+            return get_cal_datResponse(flag = "ERROR")
+
+        return get_cal_datResponse(flag = "SUCCESS")
+
+    @classmethod
+    def handle_act_linear(cls, req: act_linearRequest) -> act_linearResponse:
+        """
+        act_linear callback function. Controls movement of linear actuator.
+
+        INPUTS (act_linearRequest):
+        * actuate: identifies whether to "extend" or "retract" the linear actuator.
+        OUTPUTS (act_linearResponse):
+        * flag: description of how act_linear process went
+        """
+
+        actuate = req.actuate
+        if actuate == "extend":
+            cls.serialcomm.write(str(1).encode()) # "1" is associated to full extention (50 mm stroke)
+        elif actuate == "retract":
+            cls.serialcomm.write(str(0).encode()) # "0" is associated to full retraction (0 mm stroke)
+        else: # Otherwise throw error
+            rospy.logerr("Invalid request for actuation has likely been inputted!", "red")
+            return act_linearResponse(flag = "ERROR")
+        return act_linearResponse(flag = "SUCCESS")
+
+
+
+if __name__ == '__main__':
+    # Define nimo_end_effector node and process service calls.
+    rospy.init_node('nimo_end_effector')
+    N_sample_node = NSampleNode()
+    rospy.spin()
+    
